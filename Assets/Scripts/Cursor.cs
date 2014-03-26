58acd3608b99b05caa0b78f6eb90fef6227a4ed7
raphael goujet <raphael.goujet@gmail.com>
2014-03-26 11:06:58 +0100

    added all Unity sources

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+	//référence vers l'objet pixel
+	public InteractivePixel pixel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//stocker la position de départ du curseur
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//position du pixel
+
+		//si on appuie sur "E" le curseur va sur le pixel
+		   // les modifications de couleur s'appliquent
+
+		//si on réappuie sur "E" le curseur retourne à sa position de départ
+		   // les modifications de couleur ne s'appliquent pas
+	}
+}
---
+++ Assets/Scripts/InteractivePixel.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractivePixel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = Color.green;
+	}
+
+	private void addBlue(float delta) {
+		Color ancienneCouleur = renderer.material.color;
+		
+		//couleur avant
+		Debug.Log("avant "+ancienneCouleur);
+		
+		float nouveauBleu = ancienneCouleur.b + delta;
+		
+		if(nouveauBleu > 1f){
+			nouveauBleu = 1f;
+		}
+		
+		if(nouveauBleu < 0f){
+			nouveauBleu = 0f;
+		}
+		
+		renderer.material.color = new Color(ancienneCouleur.r,
+		                                    ancienneCouleur.g,
+		                                    nouveauBleu);
+		//couleur après
+		Debug.Log("après "+renderer.material.color);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyUp("b"))
+		{
+			addBlue(0.1f);
+		}
+		
+		if(Input.GetKeyUp("n"))
+		{
+			addBlue (-0.1f);
+		}
+
+	}
+}
