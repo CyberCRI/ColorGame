bdba3b2dca2a2705b46324a5abe67e3a63fde016
raphael goujet <raphael.goujet@gmail.com>
2014-03-26 13:55:03 +0100

    added new script to initialize target image from very small image

---
+++ Assets/Scripts/ImageInitializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageInitializer : MonoBehaviour {
+
+	public SpriteRenderer image;
+
+	// Use this for initialization
+	void Start () {
+		image.transform.localScale = new Vector3(40f, 40f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
